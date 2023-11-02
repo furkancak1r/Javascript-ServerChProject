df9dd1d63e8f03185beb5249b2e0205554029f8e
furkancak1r <furkancakr7@gmail.com>
2023-11-02 09:26:15 +0300

    Update mongoose.js

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb+srv://furkan:O6n9c2e7*@czmcluster.0h9u0ui.mongodb.net/ChProject_DB', {
+mongoose.connect('connectionString', {
 //mongoose.connect('mongodb://localhost:27017/testapii', {
 	useNewUrlParser: true,
 	useUnifiedTopology: true,
